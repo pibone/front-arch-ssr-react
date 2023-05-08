e0103f77cadb91868f0994c8593b64046ca5b655
Daniel Peña Iglesias <danpeis@gmail.com>
2023-05-08 13:16:54 +0200

    chore: updated next@13.4 & SCSS support

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import '@/styles/main.scss'
+
+export const metadata = {
+    title: 'Next SSR Archetype',
+    description: 'Sample skeleton webpage',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        <>
+            <h1 className="bg-green-300 border-green-600 border-b p-4 m-4 rounded">
+                Hello, Next.js archetype!
+            </h1>
+        </>
+    )
+}
