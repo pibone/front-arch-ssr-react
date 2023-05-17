a52442f2a79046e9c18f4de22647867f70bfea68
Daniel Peña Iglesias <danpeis@gmail.com>
2023-05-17 16:00:58 +0200

    revert: pact integration

--- .blueprints/api/__kebabCase_name__/__kebabCase_name__.spec.ts
+++ .blueprints/api/__kebabCase_name__/__kebabCase_name__.spec.ts
@@ -1,15 +1,4 @@
 
 import { {{ camelCase name }}ApiProto } from './{{ kebabCase name }}.api'
-import { apiPactWith } from '@/common/api/test-utils'
 import { {{ camelCase name }}Sampler } from './{{ kebabCase name }}.sample'
 
-apiPactWith({
-    pactConfig: {
-        consumer: '{{ kebabCase name }}-consumer',
-        provider: '{{ kebabCase name }}-provider',
-    },
-    endpoint: '/{{ kebabCase name }}',
-    name: '{{ pascalCase name }}',
-    createClient: (baseUrl: string) => {{ camelCase name }}ApiProto(baseUrl)
-    tests: {},
-})
