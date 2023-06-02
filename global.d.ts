20c3a6e81b5517c80ceb8c495a0232cc872977a3
Daniel Peña Iglesias <danpeis@gmail.com>
2023-06-02 12:11:54 +0200

    chore(typescript): new utility types & support for *.d.ts alongside the module files

--- global.d.ts
+++ global.d.ts
@@ -9,3 +9,108 @@ declare module '*.module.scss' {
     const classes: { readonly [key: string]: string }
     export default classes
 }
+
+declare type EmptyObject = Record<string, never>
+
+/**
+ * Opaque
+ * @desc Declares an Opaque type
+ * @see https://dev.to/stereobooster/pragmatic-types-opaque-types-and-how-they-could-have-saved-mars-climate-orbiter-1551
+ * @example
+ *   // Expect: "string | null"
+ *   NonUndefined<string | null | undefined>;
+ */
+declare type Opaque<K, T> = T & { __TYPE__: K }
+
+/**
+ * NonUndefined
+ * @desc Exclude undefined from set `A`
+ * @example
+ *   // Expect: "string | null"
+ *   NonUndefined<string | null | undefined>;
+ */
+declare type NonUndefined<A> = A extends undefined ? never : A
+
+/**
+ * FunctionKeys
+ * @desc Get union type of keys that are functions in object type `T`
+ * @example
+ *   type MixedProps = {name: string; setName: (name: string) => void; someKeys?: string; someFn?: (...args: any) => any;};
+ *
+ *   // Expect: "setName | someFn"
+ *   type Keys = FunctionKeys<MixedProps>;
+ */
+declare type FunctionKeys<T extends object> = {
+    [K in keyof T]-?: NonUndefined<T[K]> extends Function ? K : never
+}[keyof T]
+
+/**
+ * NonFunctionKeys
+ * @desc Get union type of keys that are non-functions in object type `T`
+ * @example
+ *   type MixedProps = {name: string; setName: (name: string) => void; someKeys?: string; someFn?: (...args: any) => any;};
+ *
+ *   // Expect: "name | someKey"
+ *   type Keys = NonFunctionKeys<MixedProps>;
+ */
+declare type NonFunctionKeys<T extends object> = {
+    [K in keyof T]-?: NonUndefined<T[K]> extends Function ? never : K
+}[keyof T]
+
+/**
+ * RequiredKeys
+ * @desc Get union type of keys that are required in object type `T`
+ * @see https://stackoverflow.com/questions/52984808/is-there-a-way-to-get-all-required-properties-of-a-typescript-object
+ * @example
+ *   type Props = { req: number; reqUndef: number | undefined; opt?: string; optUndef?: number | undefined; };
+ *
+ *   // Expect: "req" | "reqUndef"
+ *   type Keys = RequiredKeys<Props>;
+ */
+declare type RequiredKeys<T> = {
+    [K in keyof T]-?: {} extends Pick<T, K> ? never : K
+}[keyof T]
+
+/**
+ * OptionalKeys
+ * @desc Get union type of keys that are optional in object type `T`
+ * @see https://stackoverflow.com/questions/52984808/is-there-a-way-to-get-all-required-properties-of-a-typescript-object
+ * @example
+ *   type Props = { req: number; reqUndef: number | undefined; opt?: string; optUndef?: number | undefined; };
+ *
+ *   // Expect: "opt" | "optUndef"
+ *   type Keys = OptionalKeys<Props>;
+ */
+declare type OptionalKeys<T> = {
+    [K in keyof T]-?: {} extends Pick<T, K> ? K : never
+}[keyof T]
+
+/**
+ * PromiseType
+ * @desc Obtain Promise resolve type
+ * @example
+ *   // Expect: string;
+ *   type Response = PromiseType<Promise<string>>;
+ */
+export type PromiseType<T extends Promise<any>> = T extends Promise<infer U>
+    ? U
+    : never
+
+/**
+ * Optional
+ * @desc From `T` make a set of properties by key `K` become optional
+ * @example
+ *    type Props = {
+ *      name: string;
+ *      age: number;
+ *      visible: boolean;
+ *    };
+ *
+ *    // Expect: { name?: string; age?: number; visible?: boolean; }
+ *    type Props = Optional<Props>;
+ *
+ *    // Expect: { name: string; age?: number; visible?: boolean; }
+ *    type Props = Optional<Props, 'age' | 'visible'>;
+ */
+declare type Optional<T, K extends keyof T = keyof T> = Omit<T, K> &
+    Partial<Pick<T, K>>
